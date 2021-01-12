099feb200119180e08a6b916822666b582f60b63
kkata <14849535+kkata@users.noreply.github.com>
2021-01-12 12:02:21 +0900

    programmatic navigation

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,10 +1,21 @@
 <template>
   <p>Register for the event here</p>
+  <button @click="register">Register Me</button>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  methods: {
+    register() {
+      // Call to API
+      // Push back to event details
+
+      this.$router.push({
+        name: 'EventDetails'
+      })
+    }
+  }
 }
 </script>
 
