6ac6e4ed7cdd02ed384ec91cdefc9888f3905f97
kkata <14849535+kkata@users.noreply.github.com>
2021-05-12 20:48:42 +0900

    8. Flash Messages

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -6,10 +6,18 @@
 <script>
 export default {
   props: ['event'],
+  inject: ['GStore'],
   methods: {
     register() {
       // Call to API
       // Push back to event details
+
+      this.GStore.flashMessage =
+        'You are succesfully registerd for ' + this.event.title
+
+      setTimeout(() => {
+        this.GStore.flashMessage = ''
+      }, 3000)
 
       this.$router.push({
         name: 'EventDetails'
